3c884e3b2b2e4eb4fcc703bcbfaf28de30c2a91c
Mason Bair <mason.s.bair@gmail.com>
2024-05-29 08:32:51 -0400

    Working on forwarding a display

--- start_apache.sh
+++ start_apache.sh
@@ -4,7 +4,5 @@ conda activate pyimagej
 rm /usr/lib/x86_64-linux-gnu/libstdc++.so.6.0.28
 rm /usr/lib/x86_64-linux-gnu/libstdc++.so.6
 ln -s /opt/conda/envs/pyimagej/lib/libstdc++.so.6 /usr/lib/x86_64-linux-gnu/libstdc++.so.6
-
-ray job submit -- python macros/primes.py
 # Start Ray head in the foreground
 apachectl -D FOREGROUND
