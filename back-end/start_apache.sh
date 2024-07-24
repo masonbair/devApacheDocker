39589c38212af15e1a9bcd13c4d3af8f0b3bf926
Mason Bair <mason.s.bair@gmail.com>
2024-07-24 10:37:10 -0400

    Made the xpra-ray container about half as small.

--- back-end/start_apache.sh
+++ back-end/start_apache.sh
@@ -5,5 +5,8 @@ rm /usr/lib/x86_64-linux-gnu/libstdc++.so.6.0.28
 rm /usr/lib/x86_64-linux-gnu/libstdc++.so.6
 chmod +x /var/www/html/flask/static/Fiji.app/ImageJ-linux64
 ln -s /opt/conda/envs/pyimagej/lib/libstdc++.so.6 /usr/lib/x86_64-linux-gnu/libstdc++.so.6
+rm /var/www/html/index.html
 # Start Ray head in the foreground
 apachectl -D FOREGROUND
+a2ensite xpra-flask-apache.conf
+a2dissite 000-default.conf
--- xpra-ray/start_ray.sh
+++ xpra-ray/start_ray.sh
@@ -2,7 +2,8 @@
 micromamba activate pyimagej
 rm /tmp/.X80-lock
 XPRA_PASSWORD=$XPRA_PASSWORD
-xpra start :80 --daemon=yes --open-files=off --file-transfer=off --clipboard=no --encryption-keyfile=/xpra.txt
+xpra start :80 --daemon=yes --open-files=off --file-transfer=off --clipboard=no 
+#--encryption-keyfile=/xpra.txt
 
 # Starts ray 
 ray start --head --dashboard-host=0.0.0.0 --block #--port 6379 --num-cpus=1 --num-gpus=0 
