8f369bd18289a0a1b2ce5a9b3339c81dfb44f3ba
masonbair <mason.s.bair@gmail.com>
2024-08-06 12:20:30 +0000

    Added more comments

--- back-end/start_apache.sh
+++ back-end/start_apache.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+
+#This starts the conda environment
 micromamba activate pyimagej
+# Installs a wsgi module for Apache
 /opt/conda/envs/pyimagej/bin/mod_wsgi-express install-module
 rm /usr/lib/x86_64-linux-gnu/libstdc++.so.6.0.28
 rm /usr/lib/x86_64-linux-gnu/libstdc++.so.6
+# allows the execution of the ImageJ-Linux file, this is useful for running ImageJ
 chmod +x /var/www/html/flask/static/Fiji.app/ImageJ-linux64
+
 ln -s /opt/conda/envs/pyimagej/lib/libstdc++.so.6 /usr/lib/x86_64-linux-gnu/libstdc++.so.6
 rm /var/www/html/index.html
-# Start Ray head in the foreground
+# Start apache in the foreground
 apachectl -D FOREGROUND
+
+#If you make a new apache configuration. make sure to use the command a2ensite to have apache use it
 a2ensite xpra-flask-apache.conf
 a2dissite 000-default.conf
--- xpra-ray/start_ray.sh
+++ xpra-ray/start_ray.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+
+#activates conda environment
 micromamba activate pyimagej
+
+# Some programs have a lock file, this helps tell a program when and when not to run. 
+# So it is important to reset the lock files so all programs are able to start
 rm /tmp/.X80-lock
+
+# Sets the xpra password
 XPRA_PASSWORD=$XPRA_PASSWORD
+# starts xpra
 xpra start :80 --daemon=yes --open-files=off --file-transfer=off --clipboard=no 
 #--encryption-keyfile=/xpra.txt
 
@@ -9,7 +17,7 @@ xpra start :80 --daemon=yes --open-files=off --file-transfer=off --clipboard=no
 ray start --head --dashboard-host=0.0.0.0 --block #--port 6379 --num-cpus=1 --num-gpus=0 
 
 
-#Define cleanup procedure
+#This defines the cleanup procedure for when the container stops. Helps have an easy restart
 cleanup() {
     echo "Container stopped, performing cleanup..."
     xpra stop
