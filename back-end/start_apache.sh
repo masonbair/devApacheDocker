41ea7a0a605925d7eeac3ffc012c47015a5a895a
masonbair <mason.s.bair@gmail.com>
2025-02-27 11:26:22 +0100

    Silly error

--- back-end/start_apache.sh
+++ back-end/start_apache.sh
@@ -11,9 +11,10 @@ chmod +x /var/www/html/flask/static/Fiji.app/ImageJ-linux64
 
 ln -s /opt/conda/envs/pyimagej/lib/libstdc++.so.6 /usr/lib/x86_64-linux-gnu/libstdc++.so.6
 rm /var/www/html/index.html
-# Start apache in the foreground
-apachectl -D FOREGROUND
 
 #If you make a new apache configuration. make sure to use the command a2ensite to have apache use it
 a2ensite xpra-flask-apache.conf
 a2dissite 000-default.conf
+
+# Start apache in the foreground
+apachectl -D FOREGROUND
